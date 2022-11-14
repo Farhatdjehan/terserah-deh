5d2406e9cc92dca8d203b973fdb5a336c62a22eb
farhatdk <farhatdk98@gmail.com>
2022-11-14 12:16:38 +0700

    [FEAT] word to emoji

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import DashboardLayout from "../src/components/DashboardLayout";
 import file from "./../public/file.png";
 import create from "./../public/create.png";
 import MainMenu from "../src/components/MainMenu";
-import * as animationData from "./../src/components/lottery2.json";
 import * as rocketData from "./../src/components/rocket.json";
 import * as spaceData from "./../src/components/space.json";
 

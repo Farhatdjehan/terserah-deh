d16587da9b023fa87366c776c67c8b465e4b331a
farhatdk <farhatdk98@gmail.com>
2022-11-22 12:40:46 +0700

    style adjust

--- pages/category.tsx
+++ pages/category.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { category } from "../src/components/common/constant";
 import DashboardLayout from "../src/components/DashboardLayout";
 import styles from "././../styles/pages/Category.module.scss";
+import menu from "./../public/assets/png/menu.png";
 
 export default function Category() {
   return (
@@ -12,7 +13,9 @@ export default function Category() {
           return (
             <Link key={index} href={`/random/${index}?${item?.category}`}>
               <div className={styles.menu}>
-                <div className={styles.images}></div>
+                <div className={styles.images}>
+                  <Image src={menu} />
+                </div>
                 <div className={styles.wrapText}>
                   <div className={styles.title}>{item?.category}</div>
                   <div className={styles.sub}>{item?.sub}</div>
